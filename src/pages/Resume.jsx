23890360110ee5acee0a0ce98e632aa147cf4405
Cody Kilpatrick <2489375+Codykilpatrick@users.noreply.github.com>
2023-01-28 12:31:25 -0500

    add link to resume

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,6 +1,13 @@
+
+
 function Resume() {
   return (
-    <h1>Resume</h1>
+    <>
+      <h1>Resume</h1>
+      <a href="https://docs.google.com/document/d/1r_B6WYMWsCdp1fnYpk1RhBZJ7fRrRBzpxH9qUVLxJUg/edit?usp=sharing">
+        Click here to download my resume!
+      </a>
+    </>
   )
 }
 
