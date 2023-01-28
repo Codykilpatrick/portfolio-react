d7aa6b4882a9346e3efcf2dee9593fb945f48291
Cody Kilpatrick <2489375+Codykilpatrick@users.noreply.github.com>
2023-01-28 11:36:15 -0500

    add navbar

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import Home from './pages/Home';
 import About from './pages/About';
 import Contact from './pages/Contact';
 import Projects from './pages/Projects';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <>
+    <NavBar />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/about' element={<About />} />
