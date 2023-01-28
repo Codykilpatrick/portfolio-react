0e4b0727a001e8620ea836d480f0c5e9443c558c
Cody Kilpatrick <2489375+Codykilpatrick@users.noreply.github.com>
2023-01-28 11:48:49 -0500

    add image to homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,12 @@
+
+
 function Home() {
   return (
-    <h1>Home</h1>
+    <>
+      <h1>Cody Kilpatrick</h1>
+      <img src="https://i.imgur.com/RsjAT9l.jpg" alt="Cody Kilpatrick headshot" />
+      <h2>U.S Navy Veteran turned Software Engineer</h2>
+    </>
   )
 }
 
