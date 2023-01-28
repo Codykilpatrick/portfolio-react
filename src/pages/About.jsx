4d4adb843f69b05fdc2793986d97593168561ede
Cody Kilpatrick <2489375+Codykilpatrick@users.noreply.github.com>
2023-01-28 12:14:43 -0500

    update about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,6 +1,23 @@
 function About() {
   return (
+    <>
       <h1>About</h1>
+      <p>
+        Welcome! I have spent the last nine years serving the U.S Navy as a Sonar Technician on surface ships. I was on two different ships and did three deployments all to the middle-east. After that I went to two different shore facilities in Virginia and Japan!
+      </p>
+      <p>
+        I am proud to have served but at the end of the day I didn't want to be apart from my wife and our puppy Sushi anymore. In the final months of service I decided to take the dive into Software Engineering!
+      </p>
+      <p>
+        Right now I want to take my new found skills and create products that will make life easier and more efficient for sailors. I think right now with the skills I am learning combined with my previous experience gives me the ability to create actionable tools for sailors everywhere.
+      </p>
+      <h2>Hobbies</h2>
+        <p>Video games</p>
+        <p>Working out</p>
+        <p>Cars</p>
+        <p>Food</p>
+        <p>Machine Learning</p>
+    </>
   )
 }
 
