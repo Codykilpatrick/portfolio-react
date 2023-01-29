0456b8b7e6deb151900f4a6e47f4786cacf2c36c
Cody Kilpatrick <2489375+Codykilpatrick@users.noreply.github.com>
2023-01-29 13:49:52 -0500

    add initial styling to contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,10 +1,11 @@
 function Contact() {
   return (
     <>
-    <h1>Reach out if you would like to work together or just talk!</h1>\
-    
-    <a href="https://www.linkedin.com/in/codykilpatrick/">Linkedin</a>
-    <a href="https://github.com/Codykilpatrick">GitHub</a>
+    <h1>Reach out if you would like to work together or just talk!</h1>
+    <div id="contact-container">
+      <a href="https://www.linkedin.com/in/codykilpatrick/">Linkedin</a>
+      <a href="https://github.com/Codykilpatrick">GitHub</a>
+    </div>
     </>
   )
 }
