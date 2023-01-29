54ea405d217b807fa477445b52bfebe25e1f8afa
Cody Kilpatrick <2489375+Codykilpatrick@users.noreply.github.com>
2023-01-29 14:19:43 -0500

    add basics to projects page

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,7 +1,18 @@
+import projects from "../data/projects"
+
 
 function Projects() {
   return (
+    <>
     <h1>Projects</h1>
+    <ul>
+      {projects.map(project => 
+        <li>
+          {project.title}
+        </li>
+      )}
+    </ul>
+    </>
   )
 }
 
