713996bbf6da29084873237990145ddad9148236
Cody Kilpatrick <2489375+Codykilpatrick@users.noreply.github.com>
2023-01-29 13:47:41 -0500

    add links to contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,6 +1,11 @@
 function Contact() {
   return (
-    <h1>Contact</h1>
+    <>
+    <h1>Reach out if you would like to work together or just talk!</h1>\
+    
+    <a href="https://www.linkedin.com/in/codykilpatrick/">Linkedin</a>
+    <a href="https://github.com/Codykilpatrick">GitHub</a>
+    </>
   )
 }
 
