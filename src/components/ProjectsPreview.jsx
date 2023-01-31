e5e8a2ab1c68758374f8fd7107fb80366c15d27a
Cody Kilpatrick <2489375+Codykilpatrick@users.noreply.github.com>
2023-01-30 19:54:11 -0500

    style projects preview page

--- src/components/ProjectsPreview.jsx
+++ src/components/ProjectsPreview.jsx
@@ -2,21 +2,18 @@ import ProjectsMain from "./ProjectsMain";
 
 const ProjectsPreview = (props) => {
   return (
-    <div>
-    <img 
-        src={props.project.image} 
-        alt={props.project.title}
-    />
-
-    <section>
+    <div id="preview-container">
+      <img 
+          src={props.project.image} 
+          alt={props.project.title}
+      />
+      <section>
         <h3>
             {props.project.title}
         </h3>
-				<button>
-            Learn more
-        </button>
-    </section>
-</div>
+				<p>{props.project.description}</p>
+      </section>
+    </div>
   );
 }
 
