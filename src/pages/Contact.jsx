ba1cdd48079f6f50194048508ab3800ecd39ed6e
Cody Kilpatrick <2489375+Codykilpatrick@users.noreply.github.com>
2023-01-31 20:56:02 -0500

    add link for email

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -3,6 +3,7 @@ function Contact() {
     <>
     <h1>Reach out if you would like to work together or just talk!</h1>
     <div id="contact-container">
+      <a href="mailto:codykilpatrick1996@gmail.com">Email</a>
       <a href="https://www.linkedin.com/in/codykilpatrick/">Linkedin</a>
       <a href="https://github.com/Codykilpatrick">GitHub</a>
     </div>
