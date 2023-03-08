f90159cf6881d7c93fd66ddf8d5447b2d3ed2283
Cody Kilpatrick <Codykilpatrick1996@gmail.com>
2023-03-08 07:49:46 -0500

    update projects page

--- src/components/ProjectsPreview.jsx
+++ src/components/ProjectsPreview.jsx
@@ -1,16 +1,19 @@
 const ProjectsPreview = (props) => {
   return (
-    <div id="preview-container">
-      <img 
-          src={props.project.image} 
-          alt={props.project.title}
-      />
-      <section>
-        <h3>
-            {props.project.title}
-        </h3>
-      </section>
-    </div>
+    <section className="project">
+      <h2>
+          {props.project.title}
+      </h2>
+      <div id="preview-container">
+        <img 
+            src={props.project.image} 
+            alt={props.project.title}
+        />
+        <p>{props.project.description}</p>
+        <h3>Technologies</h3>
+        <p>{props.project.technologies}</p>
+      </div>
+    </section>
   );
 }
 
--- src/data/projects.js
+++ src/data/projects.js
@@ -1,11 +1,20 @@
 const projects = 
 [
   {
+    title: "Whale-Tracker",
+    description: "Whale Tracker is perfect for anyone who needs to access ocean data. Whether they're a professional oceanographer or just a curious beachgoer. With its easy-to-use interface and powerful data capabilities, it's the perfect tool for anyone who wants to explore the mysteries of the ocean.",
+    image: "https://i.imgur.com/XcGUOyd.png",
+    repositoryLink: "https://github.com/Codykilpatrick/whale-tracker-front",
+    deploymentLink: "https://whale-tracker.netlify.app/",
+    technologies: "Amentum.io API, Typescript, React, PostgreSQL, Plotly, Mapbox, Sequelize, Postman"
+  },
+  {
     title: "Sea Force Coordinator",
     description: "This tool is created for both decision makers and wargamers. It is an organizational tool built with the MEN stack and allows users to create their own Ships and groups of ships and move them across the globe.",
     image: "https://i.imgur.com/FonEpSe.png",
     repositoryLink: "https://github.com/Codykilpatrick/ship-planner",
     deploymentLink: "https://ship-planner.fly.dev/",
+    technologies: "JavaScript, Node.js, MongoDB, Express, Mongoose"
   },
   {
     title: "Viking Chess",
@@ -13,6 +22,7 @@ const projects =
     image: "https://i.imgur.com/6HjgD9J.png",
     repositoryLink: "https://github.com/Codykilpatrick/viking-chess",
     deploymentLink: "https://codykilpatrick-viking-chess.netlify.app/",
+    technologies: "JavaScript, CSS, HTML, Git"
   },
   {
     title: "Tackling Power Line faults with supervised Machine Learning",
@@ -20,6 +30,7 @@ const projects =
     image: "https://i.imgur.com/SkgYh3O.png",
     repositoryLink: "https://github.com/Codykilpatrick/Supervised-ML-Classification",
     deploymentLink: "https://github.com/Codykilpatrick/Supervised-ML-Classification",
+    technologies: "Scikit-Learn, Python, Jupyter notebook, Seaborn"
   },
   {
     title: "Eve Marketeer",
@@ -27,6 +38,7 @@ const projects =
     image: "https://i.imgur.com/q2vu8Ux.png",
     repositoryLink: "https://github.com/Codykilpatrick/Eve-marketeer",
     deploymentLink: "https://github.com/Codykilpatrick/Eve-marketeer",
+    technologies: "Python, Numpy, Pandas"
   },
 ]
 
