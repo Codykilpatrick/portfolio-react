52317f90f019b7c6fb3767a3061eafda09e2aa02
Cody Kilpatrick <Codykilpatrick1996@gmail.com>
2023-03-13 09:12:08 -0400

    update about section with images

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,24 +1,30 @@
 function About() {
   return (
     <>
-    <div className="about">
       <h1>About</h1>
-      <p>
-        Welcome! I have spent the last nine years serving the U.S Navy as a Sonar Technician on surface ships. I was on two different ships and did three deployments all to the middle-east. After that I went to two different shore facilities in Virginia and Japan!
-      </p>
-      <p>
-        I am proud to have served but at the end of the day I didn't want to be apart from my wife and our puppy Sushi anymore. In the final months of service I decided to take the dive into Software Engineering!
-      </p>
-      <p>
-        Right now I want to take my new found skills and create products that will make life easier and more efficient for sailors. I think right now with the skills I am learning combined with my previous experience gives me the ability to create actionable tools for sailors everywhere.
-      </p>
-      <h2>Hobbies</h2>
-        <p>Video games</p>
-        <p>Working out</p>
-        <p>Cars</p>
-        <p>Food</p>
-        <p>Machine Learning</p>
-    </div>
+      <div id="about-container">
+        <div className="about">
+          <div id="image-container">
+            <img src="https://i.imgur.com/bk5lvN3.jpg" alt="2016 Corvette"/>
+            <img src="https://i.imgur.com/ay3WTJG.jpg" alt="Man and dog"/>
+            <img src="https://i.imgur.com/sZlAnYu.jpg" alt="Dog on boat in lifejacket"/>
+            <img src="https://i.imgur.com/Z4qxF5P.jpg" alt="Man woman and dog on boat"/>
+            <img src="https://i.imgur.com/GpTLoi2.jpg" alt="Dog and woman in car"/>
+            <img src="https://i.imgur.com/UVBaUds.jpg" alt="Man and woman at wedding"/>
+          </div>
+          <div id="about-text">
+            <p>
+              Welcome! I have spent the last nine years serving the U.S Navy as a Sonar Technician on surface ships. I was on two different ships and did three deployments all to the middle-east. After that I went to two different shore facilities in Virginia and Japan!
+            </p>
+            <p>
+              I am proud to have served but at the end of the day I didn't want to be apart from my wife and our puppy Sushi anymore. In the final months of service I decided to take the dive into Software Engineering!
+            </p>
+            <p>
+              Right now I want to take my new found skills and create products that will make life easier and more efficient for sailors. I think right now with the skills I am learning combined with my previous experience gives me the ability to create actionable tools for sailors everywhere.
+            </p>
+          </div>
+        </div>  
+      </div>
     </>
   )
 }
